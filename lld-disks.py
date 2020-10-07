bc626323532ef7542272b4635aefd210e60825eb
Lenon Corrêa <lenonac_@hotmail.com>
2020-10-07 16:18:01 -0300

    remove disk partitions

--- lld-disks.py
+++ lld-disks.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python
 import os
 import json
+import re
+
+disks = []
+def remove_number(devices):
+    for device in devices:
+        r = re.compile(r'([0-9])|([\-])')
+        device = r.sub('',device)
+        if device not in disks:
+            disks.append(device)
+    return(disks)
 
 if __name__ == "__main__":
-    # Iterate over all block devices, but ignore them if they are in the
-    # skippable set
     skippable = ("sr", "loop", "ram")
     devices = (device for device in os.listdir("/sys/class/block")
                if not any(ignore in device for ignore in skippable))
+    devices = remove_number(devices)
     data = [{"{#DEVICENAME}": device} for device in devices]
     print(json.dumps({"data": data}, indent=4))
