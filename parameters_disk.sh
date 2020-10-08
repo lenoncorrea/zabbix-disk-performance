4cbc1112f1dcef35b1bae3daf2670435033a209d
Lenon Corrêa <lenonac_@hotmail.com>
2020-10-08 10:19:14 -0300

    working on dynamic script to capture arguments from disk

--- parameters_disk.sh
+++ parameters_disk.sh
@@ -2,8 +2,9 @@
 
 first_line=$(smartctl -a /dev/sda | grep -i -n -w "ATTRIBUTE_NAME" | cut -f1 -d ":") # DEVOLVE NUMERO DA LINHA ONDE COMEÇAM PARAMETROS
 last_line=$(smartctl -a /dev/sda | grep -i -n -w "SMART Error Log Version" | cut -f1 -d ":") # DEVOLVE NUMERO DA LINHA ONDE TERMINAM PARAMETROS
+last_line=$(($last_line-2))
 
-for line in $(seq $first_line ($last_line-2))
+for line in $(seq $first_line $last_line)
 do 
   element=$(smartctl -a /dev/sda | head -$line | tail -1 | awk '{print $2}') # DEVOLVE NOME DO ELEMENTO
   echo $element
