bb8736ec888b2af60f72a7895555be383e5e75fd
Lenon Corrêa <lenonac_@hotmail.com>
2020-10-08 10:31:36 -0300

    working on dynamic script to capture arguments from disk

--- parameters_disk.sh
+++ parameters_disk.sh
@@ -4,12 +4,11 @@ first_line=$(smartctl -a /dev/sda | grep -i -n -w "ATTRIBUTE_NAME" | cut -f1 -d 
 first_line=$(($first_line+1)) # AJUSTA PARA PEGAR PRIMEIRO PARAMETRO
 last_line=$(smartctl -a /dev/sda | grep -i -n -w "SMART Error Log Version" | cut -f1 -d ":") # DEVOLVE NUMERO DA LINHA ONDE TERMINAM PARAMETROS
 last_line=$(($last_line-2)) # AJUSTA PARA REMOVER LINHAS EM BRANCO
-data=()
 
 for line in $(seq $first_line $last_line)
 do 
   element=$(smartctl -a /dev/sda | head -$line | tail -1 | awk '{print $2}') # DEVOLVE NOME DO ELEMENTO
-  data+=('$element')
+  set -A data '$element'
 done
 
 echo $data
