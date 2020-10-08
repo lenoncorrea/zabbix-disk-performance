94b806c97d78dfc05c923d43f9f19d4332e538d7
Lenon Corrêa <lenonac_@hotmail.com>
2020-10-08 10:50:20 -0300

    working on dynamic script to capture arguments from disk

--- parameters_disk.sh
+++ parameters_disk.sh
@@ -7,8 +7,15 @@ last_line=$(($last_line-2)) # AJUSTA PARA REMOVER LINHAS EM BRANCO
 
 for line in $(seq $first_line $last_line)
 do 
-  element=$(smartctl -a /dev/sda | head -$line | tail -1 | awk '{print $2}') # DEVOLVE NOME DO ELEMENTO
-  data[$line]="$element"
+  elements=$(smartctl -a /dev/sda | head -$line | tail -1 | awk '{print $2}') # DEVOLVE NOME DO ELEMENTO
+  data[$line]="$elements"
 done
+#echo ${data[@]}
 
+for element in $elements
+do
+  data = [{"{#PARAMETER}": $element} 
+done
+# data = [{"{#DEVICENAME}": device} for device in devices]
+# print(json.dumps({"data": data}, indent=4))
 echo ${data[@]}
