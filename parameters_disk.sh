4e1a050ad5abe6519335eb34b1c8464abfbddf3a
Lenon Corrêa <lenonac_@hotmail.com>
2020-10-08 14:36:06 -0300

    add archive sudoers and add sudo in commands smartctl

--- parameters_disk.sh
+++ parameters_disk.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-first_line=$(smartctl -a /dev/sda | grep -i -n -w "ATTRIBUTE_NAME" | cut -f1 -d ":") # DEVOLVE NUMERO DA LINHA ONDE COMEÇAM PARAMETROS
+first_line=$(sudo smartctl -a /dev/sda | grep -i -n -w "ATTRIBUTE_NAME" | cut -f1 -d ":") # DEVOLVE NUMERO DA LINHA ONDE COMEÇAM PARAMETROS
 first_line=$(($first_line+1)) # AJUSTA PARA PEGAR PRIMEIRO PARAMETRO
-last_line=$(smartctl -a /dev/sda | grep -i -n -w "SMART Error Log Version" | cut -f1 -d ":") # DEVOLVE NUMERO DA LINHA ONDE TERMINAM PARAMETROS
+last_line=$(sudo smartctl -a /dev/sda | grep -i -n -w "SMART Error Log Version" | cut -f1 -d ":") # DEVOLVE NUMERO DA LINHA ONDE TERMINAM PARAMETROS
 last_line=$(($last_line-2)) # AJUSTA PARA REMOVER LINHAS EM BRANCO
 
 for line in $(seq $first_line $last_line)
 do 
-  elements=$(smartctl -a /dev/sda | head -$line | tail -1 | awk '{print $2}') # DEVOLVE NOME DO ELEMENTO
+  elements=$(sudo smartctl -a /dev/sda | head -$line | tail -1 | awk '{print $2}') # DEVOLVE NOME DO ELEMENTO
   data[$line]="$elements"
 done
 #echo ${data[@]}
