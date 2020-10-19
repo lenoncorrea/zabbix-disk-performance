ca7469bfccf7511ebf33cb14b82e2201596a0920
Lenon Corrêa <lenonac_@hotmail.com>
2020-10-19 14:39:39 -0300

    ajuste in shell script

--- parameters_disk.sh
+++ parameters_disk.sh
@@ -6,9 +6,9 @@ first_line=$(sudo /usr/sbin/smartctl -a /dev/$disk | grep -i -n -w "ATTRIBUTE_NA
 first_line=$(($first_line+1)) # AJUSTA PARA PEGAR PRIMEIRO PARAMETRO
 last_line=$(sudo /usr/sbin/smartctl -a /dev/$disk | grep -i -n -w "SMART Error Log Version" | cut -f1 -d ":") # DEVOLVE NUMERO DA LINHA ONDE TERMINAM PARAMETROS
 last_line=$(($last_line-2)) # AJUSTA PARA REMOVER LINHAS EM BRANCO
-device_model=$(sudo /usr/sbin/smartctl -a /dev/sda | grep 'Device Model' | cut -f2 -d ":")
-serial_number=$(sudo /usr/sbin/smartctl -a /dev/sda | grep 'Serial Number' | cut -f2 -d ":")
-disk_capacity=$(sudo /usr/sbin/smartctl -a /dev/sda | grep 'User Capacity' | cut -f2 -d ":" | cut -f2 -d "[" | cut -f1 -d "]")
+device_model=$(sudo /usr/sbin/smartctl -a /dev/$disk | grep 'Device Model' | cut -f2 -d ":")
+serial_number=$(sudo /usr/sbin/smartctl -a /dev/$disk | grep 'Serial Number' | cut -f2 -d ":")
+disk_capacity=$(sudo /usr/sbin/smartctl -a /dev/$disk | grep 'User Capacity' | cut -f2 -d ":" | cut -f2 -d "[" | cut -f1 -d "]")
 
 for line in $(seq $first_line $last_line)
 do 
@@ -17,4 +17,4 @@ do
   data="$data,"'{"{#PARAMETER}":"'$elements'"}'
 done
 done < /etc/zabbix/zabbix_agentd.d/zabbix-disk-performance/disks.txt
-echo '{"data":[{"disco": [{"{#DISK}":"'$disk'"},{"{#MODEL}": "'$device_model'"},{"{#SERIAL}": "'$serial_number'"},{"{#CAPACITY}": "'$disk_capacity'"},]},{"parameters": ['${data#,}']}]}' >> /etc/zabbix/zabbix_agentd.d/zabbix-disk-performance/$disk
+echo '{"data":[{"disco": [{"{#DISK}":"'$disk'"},{"{#MODEL}": "'$device_model'"},{"{#SERIAL}": "'$serial_number'"},{"{#CAPACITY}": "'$disk_capacity'"},]},{"parameters": ['${data#,}']}]}' > /etc/zabbix/zabbix_agentd.d/zabbix-disk-performance/$disk
