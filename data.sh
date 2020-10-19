bb13af6b076d8aaf79f5fed035a81e635c5bca7a
Lenon Corrêa <lenonac_@hotmail.com>
2020-10-19 14:59:42 -0300

    ajuste in shell script

--- data.sh
+++ data.sh
@@ -2,5 +2,5 @@
 
 while read line
 do
-  ruturn=$("cat /etc/zabbix/zabbix_agentd.d/zabbix-disk-performance/'$line'")
+  ruturn=$("cat /etc/zabbix/zabbix_agentd.d/zabbix-disk-performance/$line")
 done < /etc/zabbix/zabbix_agentd.d/zabbix-disk-performance/disks.txt
