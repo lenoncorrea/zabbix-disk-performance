c716e9d1ab1db775f7526d58ae4cfb14278f73ae
Lenon Corrêa <lenonac_@hotmail.com>
2020-10-19 16:00:11 -0300

    ajuste in shell script

--- data.sh
+++ data.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-while read line
-do
-  disk=$line
-  data=$(cat /etc/zabbix/zabbix_agentd.d/zabbix-disk-performance/$disk)
-  echo $data
-done < /etc/zabbix/zabbix_agentd.d/zabbix-disk-performance/disks.txt
+disk=$1
+data=$(cat /etc/zabbix/zabbix_agentd.d/zabbix-disk-performance/$disk)
+echo $data
