ad27d9de1f899a80eda69fdee3a79417cc7e47fb
Lenon Corrêa <lenonac_@hotmail.com>
2020-10-19 17:12:43 -0300

    adjuste general

--- discover-parameters.sh
+++ discover-parameters.sh
@@ -5,12 +5,12 @@ device_model=$(sudo /usr/sbin/smartctl -a /dev/$disk | grep 'Device Model' | cut
 if [[ $device_model == "KINGSTON" ]]
 then
   value=$(sudo /usr/sbin/smartctl -a /dev/$disk | grep "SSD_Life_Left" | awk '{print $$10}')
-  echo $device_model
+  echo $value
   exit
 elif [[ $device_model == "ST1000DM003-1CH162" ]]
 then
   value=$(sudo /usr/sbin/smartctl -a /dev/$disk | grep "Load_Cycle_Count" | awk '{print $$10}')
-  echo $device_model
+  echo $value
   exit
 else
   echo "Not value for disk"
