6b8e6feae5228b4e21b401dcf528a6bae9f9b711
Lenon Corrêa <lenonac_@hotmail.com>
2020-10-19 11:59:35 -0300

    adjsut in userparameter

--- discover-disks.py
+++ discover-disks.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
                if not any(ignore in device for ignore in skippable))
     devices = remove_number(devices)
     file = open("/etc/zabbix/zabbix_agentd.d/zabbix-disk-performance/disks.txt","w")
-    file.write(devices)
+    file.write(''+devices+'')
     file.close()
     # data = [{"{#DEVICENAME}": device} for device in devices]
     # print(json.dumps({"data": data}, indent=4))
