5c25b55cfe510917aa3fec2051d991cd5054d97b
Lenon Corrêa <lenonac_@hotmail.com>
2020-10-20 11:27:28 -0300

    adjust in health-disk, and add script for check temperature

--- temperature-disk.sh
+++ temperature-disk.sh
@@ -4,22 +4,22 @@ disk=$1
 device_model=$(sudo /usr/sbin/smartctl -a /dev/$disk | grep 'Device Model' | cut -f2 -d ":" | awk '{print $1}')
 if [[ $device_model == "CT120BX500SSD1" ]] # CRUCIAL BX500 120G
 then
-  value=$(sudo /usr/sbin/smartctl -a /dev/$disk | grep "Percent_Lifetime_Remain" | awk '{print $4}')
+  value=$(sudo /usr/sbin/smartctl -a /dev/$disk | grep "Temperature_Celsius" | awk '{print $10}')
   echo $value
   exit
 elif [[ $device_model == "CT1000MX500SSD1" ]] # CRUCIAL MX500 1T
 then
-  value=$(sudo /usr/sbin/smartctl -a /dev/$disk | grep "Percent_Lifetime_Remain" | awk '{print $4}')
+  value=$(sudo /usr/sbin/smartctl -a /dev/$disk | grep "Temperature_Celsius" | awk '{print $10}')
   echo $value
   exit
 elif [[ $device_model == "CT2000MX500SSD1" ]] # CRUCIAL MX500 2T
 then
-  value=$(sudo /usr/sbin/smartctl -a /dev/$disk | grep "Percent_Lifetime_Remain" | awk '{print $4}')
+  value=$(sudo /usr/sbin/smartctl -a /dev/$disk | grep "Temperature_Celsius" | awk '{print $10}')
   echo $value
   exit
 elif [[ $device_model == "Samsung" ]] # SAMSUNG EVO850 1TB
 then
-  value=$(sudo /usr/sbin/smartctl -a /dev/$disk | grep "Wear_Leveling_Count" | awk '{print $4}')
+  value=$(sudo /usr/sbin/smartctl -a /dev/$disk | grep "Airflow_Temperature_Cel" | awk '{print $10}')
   echo $value
   exit
 else
