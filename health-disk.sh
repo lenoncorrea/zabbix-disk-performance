133eaea2d16f9d27483b6ab7ce510ede101bbb2b
Lenon Corrêa <lenonac_@hotmail.com>
2020-10-20 11:19:01 -0300

    adjust in health disk, parameter for health

--- health-disk.sh
+++ health-disk.sh
@@ -4,17 +4,17 @@ disk=$1
 device_model=$(sudo /usr/sbin/smartctl -a /dev/$disk | grep 'Device Model' | cut -f2 -d ":" | awk '{print $1}')
 if [[ $device_model == "CT120BX500SSD1" ]] # CRUCIAL BX500 120G
 then
-  value=$(sudo /usr/sbin/smartctl -a /dev/$disk | grep "Percent_Lifetime_Remain" | awk '{print $10}')
+  value=$(sudo /usr/sbin/smartctl -a /dev/$disk | grep "Percent_Lifetime_Remain" | awk '{print $4}')
   echo $value
   exit
 elif [[ $device_model == "CT1000MX500SSD1" ]] # CRUCIAL MX500 1T
 then
-  value=$(sudo /usr/sbin/smartctl -a /dev/$disk | grep "Percent_Lifetime_Remain" | awk '{print $10}')
+  value=$(sudo /usr/sbin/smartctl -a /dev/$disk | grep "Percent_Lifetime_Remain" | awk '{print $4}')
   echo $value
   exit
 elif [[ $device_model == "CT2000MX500SSD1" ]] # CRUCIAL MX500 2T
 then
-  value=$(sudo /usr/sbin/smartctl -a /dev/$disk | grep "Percent_Lifetime_Remain" | awk '{print $10}')
+  value=$(sudo /usr/sbin/smartctl -a /dev/$disk | grep "Percent_Lifetime_Remain" | awk '{print $4}')
   echo $value
   exit
 elif [[ $device_model == "Samsung" ]] # SAMSUNG EVO850 1TB
